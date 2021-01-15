5bd4fbf5030bf20b8cb94b70d148881b486cc02c
phsw <pedromadureira000@gmail.com>
2021-01-15 10:38:31 -0300

    Primeiro Envio do projeto Python

---
+++ POO/Classe e objeto/atributo de classe.py
@@ -0,0 +1,28 @@
+class Pessoa:
+    olhos = 2  #esse é um atributo default ou atributo de classe. Não aloca na memoria para cada instancia da classe
+    def __init__(self, *filhos, nome=None):
+        self.nome = nome
+        self.filhos = list(filhos)
+pedro = Pessoa()
+print(Pessoa.olhos)
+print(pedro.olhos)        #o valor pode ser retornado pela classe ou objeto da classe.
+
+print(id(Pessoa.olhos))
+print(id(pedro.olhos))  #o atributo da classe é o mesmo para a classe e seu objeto instanciado.
+
+print(pedro.__dict__)  #o dunderdict faz referencia apenas aos atributos de instancia
+print(Pessoa.__dict__) #dunderdict na classe mostra os atributos de "object" e tambem mostra o atributo da classe
+
+pedro.olhos = 1
+#uma vez que eu mudei o valor do atributo de classe para um objeto, ele passa
+#a fazer parte dos atributos de instancia dessa objeto
+print(pedro.__dict__)
+print(id(pedro.olhos),id(Pessoa.olhos))
+# O interpretador python busca primeiro nos atributos da instancia, depois nos da classe.
+del pedro.olhos
+#se vc deleta o atributo da instancia, sera retornado o de classe
+print(pedro.olhos)
+
+Pessoa.olhos = 3  #vc pode alterar o atributo da classe
+print(pedro.olhos)   #nesse caso todos os objetos vao obter esse atributo, pois eles fazem referencia a ele
+
--- POO/Classe e objeto/atributo de instancia.py
+++ POO/Classe e objeto/atributo de instancia.py
@@ -3,7 +3,6 @@ class Pessoa:
         self.nome = nome
         self.filhos = list(filhos)
 
-
     def cumprimentar(self):
         return f"ola {id(self)}"
 
@@ -11,7 +10,9 @@ if __name__ == '__main__':
     pedro = Pessoa(nome="Pedro")
     henrique = Pessoa(pedro, nome="Henrique")
 
-
     henrique.sobrenome = "Pedro"
     print(henrique.__dict__)
-    print(pedro.__dict__)            #printa um dicionario com os atributos da instancia (atributos de dados apenas)
+    print(pedro.__dict__)
+#printa um dicionario com os atributos da instancia (atributos de dados apenas)
+#são os atributos que foram inicializados no objeto em __init__ e os
+#atributos adicionados dinamicamente
