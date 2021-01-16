18b35c486d3fe4531f6cb91d64ce4efb48403725
phsw <pedromadureira000@gmail.com>
2021-01-16 09:42:07 -0300

    metodos e atributos em "conceitual" metodo de classe em OO

--- POO/Classe e objeto/metodo de classe.py
+++ POO/Classe e objeto/metodo de classe.py
@@ -1,12 +1,20 @@
 class Pessoa:
-    def cumprimentar(self):
+    olhos = 2
+    def cumprimentar(self):          #metodo de instancia, acrescenta self automaticamente
         return f"ola {id(self)}"
-
+    @staticmethod
+    def metodo_statico():           #metodo statico: funciona como uma função atrelada ao objeto Pessoa
+        return 42
+    @classmethod
+    def metodo_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 if __name__ == '__main__':
     p = Pessoa()
-    print(Pessoa.cumprimentar(p))    #vc pode chamar o metodo apartir da classe, passando um objeto
-                                     # dessa classe como parametro self
-
-    print(p.cumprimentar())          #quando vc chama o metodo apartir do objeto vc nao precisa passar o
-                                     # objeto como parametro(self), ele sera passado implicitamente
+    print(Pessoa.cumprimentar(p))
+    print(p.cumprimentar())
     print(id(p))
+    #vc pode chamar o metodo apartir da classe, passando um objeto dessa classe como parametro self
+    # quando vc chama o metodo apartir do objeto vc nao precisa passar o objeto como parametro(self)
+    #ele sera passado implicitamente
+    print(p.metodo_statico(), Pessoa.metodo_statico())
+    print(p.metodo_de_classe(), Pessoa.metodo_de_classe())
---
+++ conceitual/methods and attributes.py
@@ -0,0 +1,2 @@
+#metodos e atributos de classe != metodos e atributos de instancia
+"a instancia busca nela mesma o metodo e atributo, caso nao encontre procura na classe"
