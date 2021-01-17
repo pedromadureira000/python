f64a55440ab5af66756b459522e792137e46595d
phsw <pedromadureira000@gmail.com>
2021-01-17 13:15:52 -0300

    doctestecarro.py: exercicio desdenvolvido até "direcao = Direcao()". Criado pasta "matematica" com explicação da função built-in "max()"

--- POO/doctest/doctestecarro.py
+++ POO/doctest/doctestecarro.py
@@ -90,5 +90,17 @@ O   L
 """
 
 class Motor:
+    def __init__(self):
+        self.velocidade = 0
+
     pass
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0,self.velocidade)
+
+
 
