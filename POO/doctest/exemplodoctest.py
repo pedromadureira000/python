07ab6e1c13235abd19675b11c3089ca221b4fadb
phsw <pedromadureira000@gmail.com>
2021-01-17 11:59:02 -0300

    docteste em POO. E exercicio carro resolvido em Exercicios. Atalhos atualizado: (alt + enter)

---
+++ POO/doctest/exemplodoctest.py
@@ -0,0 +1,13 @@
+"""
+Doctest é uma forma de executar codigo python do __doc__, ele executa como se fosse em um console interativo
+e retorna apenas os erros que são gerados.
+
+COMANDO PARA RODAR O DOCTEST: python3 -m doctest -v teste.py
+HELP: python3 -m doctest --help
+
+    Exemplo:
+    >>> #testes
+    >>> motor = Motor()
+    >>> motor.velocidade
+    0
+"""
--- teste.py
+++
@@ -1,6 +0,0 @@
-class Classe:
-    pass
-objeto = Classe()
-print(type(Classe))
-print(type(objeto))
-print(isinstance(objeto,object))
