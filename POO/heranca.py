e0dd4ed34adf2b701962159aa20ee940cbdcffde
phsw <pedromadureira000@gmail.com>
2021-01-17 17:44:48 -0300

    herança e edicao em conceitos.txt

---
+++ POO/heranca.py
@@ -0,0 +1,10 @@
+class Classe:
+    def __init__(self,atributo1, atributo2):
+        self.atributo2 = atributo2
+        self.atributo1 = atributo1
+
+    def atributometodo(self):
+        pass
+
+class Herdada(Classe):    #herdou todos os atributos de dados e metodos
+    pass
