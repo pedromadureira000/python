4cfc5575b783a36c8d1be648b60d839aa4fc3e78
phsw <pedromadureira000@gmail.com>
2021-02-17 12:26:19 -0300

    editado duvida mock

--- duvidas/mock.py
+++ duvidas/mock.py
@@ -12,10 +12,12 @@ insteste = Teste(insobjeto)
 print(id(insteste.parobjeto) , id(insobjeto.par))
 insteste.parobjeto = 2
 print(insobjeto.par)
+assert insobjeto.par == insteste.parobjeto
 
 
 #por que em test_envio_para_base_de_usuarios.py:32 "assert len(usuarios) == enviador.enviar.call_count" nao deveira ser
 #enviador_de_spam.enviar.call_count,como que o parametro esta fazendo referencia ao outo ?
+from unittest.mock import Mock
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
@@ -27,3 +29,18 @@ def test_qtd_de_spam(sessao, usuarios):
         'corpo do email'
     )
     assert len(usuarios) == enviador.enviar.call_count
+
+
+class EnviadorDeSpam:
+    def __init__(self, sessao, enviador):
+        self.sessao = sessao
+        self.enviador = enviador
+
+    def enviar_emails(self, remetente, assunto, corpo):
+        for usuario in self.sessao.listar():
+            self.enviador.enviar(
+                remetente,
+                usuario.email,
+                assunto,
+                corpo
+            )
