fa3f155b685c0a910f8b2645b4e8d1d80d8ed7ff
phsw <pedromadureira000@gmail.com>
2021-02-22 18:37:10 -0300

    deletei tudo de conceitual e mandei pra planiha google

--- conceitual/methods and attributes.py
+++
@@ -1,2 +0,0 @@
-#metodos e atributos de classe != metodos e atributos de instancia
-"a instancia busca nela mesma o metodo e atributo, caso nao encontre procura na classe"
---
+++ z - Dicas - z/trocando valor de variaveis.py
@@ -0,0 +1,11 @@
+x = 10
+y = "oi"
+
+    #forma errada
+z = x
+x = y
+y = z
+
+    #forma certa
+
+x, y = y, x
