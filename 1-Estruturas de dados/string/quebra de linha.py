6136e594196a7232969519311b7e33e1ca176f2c
phsw <pedromadureira000@gmail.com>
2021-04-26 10:14:34 -0300

    maid um commit

--- 1-Estruturas de dados/string/quebra de linha.py
+++ 1-Estruturas de dados/string/quebra de linha.py
@@ -1,5 +1,4 @@
-# É usado junto de um texto para quebra de linhas. Se quiser usar barra invertida numa string utilize o prefixo "r" para indicar
-# ao Python que você deseja o seu texto da maneira que está, sem analisá-lo:
-print(r"C:\\temp\ovos\\foto.png")
-print("Ola\nMundo")
-print("C:\temp\novos\foto.png")  #\t da um tab, \n pula linha, \f nao existe, e da erro.
+print(r"C:\\temp\ovos\\foto.png")  #não analiza o texto em busca de caracteres especiais
+print("Ola\nMundo")   #\n pula linha
+print("C:\temp")  #\t da um tab
+print("Ola\"Mundo")   #usar '\' para escapar aspas
---
+++ teste.py
@@ -0,0 +1,2 @@
+a = "Tesxtoapafáâ"
+print(a.casefold())
