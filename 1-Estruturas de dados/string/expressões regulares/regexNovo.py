b625f5b3deb7a02f4c2e372ce59b33acafcc971a
phsw <pedromadureira000@gmail.com>
2021-04-28 11:35:26 -0300

    estudo de regex 14

--- 1-Estruturas de dados/string/expressões regulares/regexNovo.py
+++ 1-Estruturas de dados/string/expressões regulares/regexNovo.py
@@ -442,4 +442,137 @@ qf9_6H
 print(senha_forte_regexp.findall(string))
 
 
-#-----------------------------------------/EX: Senha----------------------------------------------
+#-----------------------------------------/EX: Numeros----------------------------------------------
+# https://regex101.com/r/wjfSus/1/
+import re
+
+string = '''
+Válidos
+0.0
+00.00
+000.000
++0.0
+-00.00
++000.000
+10
+50
+8.5
+-8.5
++10.5005412136
+1231345458.54654564
+-133215646589.6543215648978977
++1.11123123
+-0.154987
+1.121654987
+10.123
+10.1
+-1.1
+Inválidos
+10..2
+++1213
+--1235050
+.124546
+-.1231324
+10.
+.1
+.10
+10.
++10.
+-10.
+5a
+b5
+'''
+
+
+def is_float(string):
+    return bool(re.search(r'^[+-]?\d+(?:\.\d+)?$', string))
+
+
+def is_int(string):
+    return bool(re.search(r'^[+-]?\d+$', string))
+
+
+while True:
+    numero = input('Digite um número: ')
+
+    if is_int(numero):
+        numero = int(numero)
+        print(f'O número {numero} foi convertido para int')
+        continue
+
+    if is_float(numero):
+        numero = float(numero)
+        print(f'O número {numero} foi convertido para float')
+        continue
+
+#-----------------------------------------/EX: E-mail ----------------------------------------------
+
+"""
+Básica
+^\w+(?:[.\-+!%]\w+)*@\w+(?:[.\-]\w+)+$
+https://regex101.com/r/9s4bgv/1/
+
+Básica 2
+^[^\s@<>\(\)[\]\.]+(?:\.[^\s@<>\(\)\[\]\.]+)*@\w+(?:[\.\-_]\w+)*$
+https://regex101.com/r/mH4ChC/2/
+
+rfc 5322  (padrão)
+^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$
+https://regex101.com/r/fkxI15/1/
+"""
+
+emails = """
+Valid email addresses
+o-que_vai.te+dar+dor-de.cabeca@gmail-com-traco.com.br
+simple@example.com
+very.common@example.com
+disposable.style.email.with+symbol@example.com
+other.email-with-hyphen@example.com
+fully-qualified-domain@example.com
+user.name+tag+sorting@example.com
+x@example.com
+example-indeed@strange-example.com
+example@s.example
+a@a.com.br
+mailhost!username@example.org
+user%example.com@example.org
+email@example.com
+firstname.lastname@example.com
+email@subdomain.example.com
+firstname+lastname@example.com
+email@123.123.123.123
+"email"@example.com
+1234567890@example.com
+email@example-one.com
+_______@example.com
+email@example.name
+email@example.museum
+email@example.co.jp
+firstname-lastname@example.com
+
+Invalid email addresses
+Abc.example.com
+<aqui-te-um@email-pra-validar.com.br>
+A@b@c@example.com
+a"b(c)d,e:f;g<h>i[j\k]l@example.com
+just"not"right@example.com
+this is"not\allowed@example.com
+this\ still\"not\\allowed@example.com
+plainaddress
+#@%^%#$@#$@#.com
+@example.com
+<email@example.com>
+email.example.com
+email@example@example.com
+.email@example.com
+email.@example.com
+email..email@example.com
+あいうえお@example.com
+email@example
+email@-example.com
+email@example..com
+Abc..123@example.com
+”(),:;<>[\]@example.com
+just”not”right@example.com
+this\ is"really"not\allowed@example.com
+"""
